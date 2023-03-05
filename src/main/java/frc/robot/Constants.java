bc2856907c31bccd14600f0b1ef27649f26d440a
Alice Lin <alin23@hb.edu>
2023-03-05 14:30:39 -0500

    cleaned up code, added a lot of comments in robot container and constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,16 +31,13 @@ public class Constants {
         public static final int LEFT_FRONT_DRIVE_CAN_ID = 1;
         public static final int LEFT_BACK_DRIVE_CAN_ID = 2;
 
-        // encoder 
+        //encoder 
         public static final double ENCODER_CALIBRATION_METERS = 0.0493;
 
-        // No turning sensitivity
+        //no turning sensitivity
         public static final double MAX_TURN_SPEED = 1;
-
         public static final double TURN_SENSITIVITY = 0;
-
         public static final double SLOW_SPEED_FRACTION = 0.3;
-
 
         //path planning constants
         public static final double ks = 0.12704;
@@ -48,11 +45,11 @@ public class Constants {
         public static final double ka = 0.46642;
         public static final double kPDriveVel = 0.16761;
         public static final double kTrackwidthMeters = 0.69;
-        public static final DifferentialDriveKinematics kDriveKinematics =
-          new DifferentialDriveKinematics(kTrackwidthMeters);
+        public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
         public static final double kMaxSpeedMetersPerSecond = 3;
         public static final double kMaxAccelerationMetersPerSecondSquared = 1;
-        // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+        
+        //reasonable baseline values for a RAMSETE follower in units of meters and seconds
         public static final double kRamseteB = 2;
         public static final double kRamseteZeta = 0.7;
     }
@@ -62,16 +59,20 @@ public class Constants {
         public static final int LEFT_ELEVATOR_MOTOR_ID = 7;
         public static final int RIGHT_ELEVATOR_MOTOR_ID = 10;
         public static final int ELEVATOR_SLEW = 5;
+
+        //elevator min and max heights in meters
         public static final double MIN_ELEVATOR_HEIGHT = 0.05;
         public static final double MAX_ELEVATOR_HEIGHT = 0.75;
 
-        public static final double CONE_TOP_HEIGHT = 0.69;
-        public static final double CUBE_TOP_HEIGHT = 0.58;
-        public static final double CONE_MID_HEIGHT = 0.34;
-        public static final double CUBE_MID_HEIGHT = 0.25;
+        //elevatr heights for cone/cube for scoring low, mid, and top nodes
         public static final double CONE_LOW_HEIGHT = 0.08;
         public static final double CUBE_LOW_HEIGHT = 0.08;
+        public static final double CONE_MID_HEIGHT = 0.34;
+        public static final double CUBE_MID_HEIGHT = 0.25;
+        public static final double CONE_TOP_HEIGHT = 0.69;
+        public static final double CUBE_TOP_HEIGHT = 0.58;
 
+        //elevator heights for intaking upright cones, cones from tip, cones from phalange, cubes from ground and cone/cube from shelf
         public static final double CONE_UP_INTAKE_HEIGHT = 0.08;
         // public static final double CONE_TIP_INTAKE_HEIGHT= xx;
         // public static final double CONE_PHALANGE_INTAKE_HEIGHT = xx;
@@ -79,8 +80,8 @@ public class Constants {
         public static final double CONE_SHELF_INTAKE_HEIGHT = 0.705;
         public static final double CUBE_SHELF_INTAKE_HEIGHT = 0.508;
 
-        // 27 inches, 41.951946 encoder counts
-        public static final double ENCODER_CALIBRATION_METERS = Units.inchesToMeters(27) / 41.951946;
+        //27 inches per 41.951946 encoder counts
+        public static final double METERS_PER_REVOLUTION = Units.inchesToMeters(27) / 41.951946;
 
         public static final double HEIGHT_TOLERANCE = Units.inchesToMeters(1);
 
@@ -98,23 +99,26 @@ public class Constants {
     }
 
     public static final class ArmConstants {
-        //Arm angle in radians
+
+        public static final int ARM_MOTOR_ID = 8;
+
+        //arm min and max angles in radians
         public static final double MAX_ARM_ANGLE = Math.PI/4;
         public static final double MIN_ARM_ANGLE = -Math.PI/4 * 3;
-        //arm Mass in kg
+        //arm mass in kg
         public static final double ARM_MASS = 2.72155;
         //arm length in meters
         public static final double ARM_LENGTH = 0.72;
 
-        public static final int ARM_MOTOR_ID = 8;
-
-        public static final double CONE_TOP_ANGLE = 0.09;
-        public static final double CUBE_TOP_ANGLE = 0.06;
-        public static final double CONE_MID_ANGLE = 0.29;
-        public static final double CUBE_MID_ANGLE = 0.46;
+        //arm angles for cone/cube for scoring low, mid, and top nodes
         public static final double CONE_LOW_ANGLE = 0.03;
         public static final double CUBE_LOW_ANGLE = 0.03;
+        public static final double CONE_MID_ANGLE = 0.29;
+        public static final double CUBE_MID_ANGLE = 0.46;
+        public static final double CONE_TOP_ANGLE = 0.09;
+        public static final double CUBE_TOP_ANGLE = 0.06;
 
+        //arm angles for intaking upright cones, cones from tip, cones from phalange, cubes from ground and cone/cube from shelf
         public static final double CONE_UP_INTAKE_ANGLE = -0.24;
         // public static final double CONE_TIP_INTAKE_ANGLE = xx;
         // public static final double CONE_PHALANGE_INTAKE_ANGLE = xx;
@@ -126,19 +130,11 @@ public class Constants {
         public static final double INITIAL_OFFSET = 0.711;
 
         public static final double ANGLE_TOLERANCE = Units.degreesToRadians(2);
-        // Arm PID constants
-        
-
     }
 
     public static final class IntakeConstants {
 
-        public static final int LEFT_INTAKE_MOTOR_ID = 5;
-        //public static final int RIGHT_INTAKE_MOTOR_ID = 10;
-        public static final int PCM_CAN_ID = 3;
-        //public static final int FORWARD_CHANNEL_SOLENOID_ID = 2;
-        //public static final int REVERSE_CHANNEL_SOLENOID_ID = 3;
-        public static final int EXTEND_INTAKE_ARM_RIGHT = 2;
+        public static final int INTAKE_MOTOR_ID = 5;
         public static final double INTAKE_SLEW_RATE = 10;
         public static final double CONE_IN_SPEED = 1.0;
         public static final double CONE_OUT_SPEED = -1.0;
@@ -155,12 +151,6 @@ public class Constants {
         public static final double WAIT_TIME = 1.0;
 
     }
-
-
-    public static final class JoystickConstants {
-        // joystick ports
-        //public static final int JOYSTICK_PORT = 1;
-    } 
 
     public static final class XboxConstants {
         public static final int XBOX_DRIVER_PORT = 0;
--- src/main/java/frc/robot/subsystems/elevator/RealElevator.java
+++ src/main/java/frc/robot/subsystems/elevator/RealElevator.java
@@ -35,12 +35,12 @@ public class RealElevator implements ElevatorIO {
         elevatorEncoderRight.setPosition(0); 
         elevatorEncoderLeft.setPosition(0); 
 
-        elevatorEncoderRight.setPositionConversionFactor(ElevatorConstants.ENCODER_CALIBRATION_METERS);
-        elevatorEncoderLeft.setPositionConversionFactor(ElevatorConstants.ENCODER_CALIBRATION_METERS);
+        elevatorEncoderRight.setPositionConversionFactor(ElevatorConstants.METERS_PER_REVOLUTION);
+        elevatorEncoderLeft.setPositionConversionFactor(ElevatorConstants.METERS_PER_REVOLUTION);
 
         // dividng by 60 to convert meters per miniute to meters per seconds
-        elevatorEncoderRight.setVelocityConversionFactor(ElevatorConstants.ENCODER_CALIBRATION_METERS / 60);
-        elevatorEncoderLeft.setVelocityConversionFactor(ElevatorConstants.ENCODER_CALIBRATION_METERS / 60);
+        elevatorEncoderRight.setVelocityConversionFactor(ElevatorConstants.METERS_PER_REVOLUTION / 60);
+        elevatorEncoderLeft.setVelocityConversionFactor(ElevatorConstants.METERS_PER_REVOLUTION / 60);
 
         elevatorMotorControllerRight.follow(elevatorMotorControllerLeft);
 
