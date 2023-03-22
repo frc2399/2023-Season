52703afa04f3bb52405a755d7ac71e147ed0b6c5
Maisie <myan2023@gmail.com>
2023-03-22 18:02:16 -0400

    one and half cone auton

---
+++ src/main/java/frc/robot/commands/auton/OneAndHalfPieceEngage.java
@@ -0,0 +1,52 @@
+package frc.robot.commands.auton;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.commands.drivetrain.DriveForwardGivenDistance;
+import frc.robot.commands.drivetrain.EngageCmd;
+import frc.robot.commands.drivetrain.TurnToNAngleCmd;
+import frc.robot.commands.intake.IntakeForGivenTime;
+import frc.robot.commands.robot.PlaceConeOnNode;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.drivetrain.DriveTrain;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.intake.Intake;
+
+public class OneAndHalfPieceEngage extends SequentialCommandGroup {
+
+    public OneAndHalfPieceEngage(DriveTrain driveTrain, Intake intake, Elevator elevator, Arm arm) {
+
+        driveTrain.resetOdometry(null);
+
+        addCommands(
+            RobotContainer.resetArmAndElevatorEncoderCommand(arm, elevator),
+            new DriveForwardGivenDistance(-0.20, driveTrain),
+            new PlaceConeOnNode(intake, elevator, arm, ElevatorConstants.CONE_TOP_HEIGHT, ArmConstants.CONE_TOP_ANGLE),
+            new PrintCommand("place cone on node finished"),
+            // leaves community
+            new DriveForwardGivenDistance(-4.2, driveTrain),
+            // TODO: find actual turn angle
+            new TurnToNAngleCmd(Units.degreesToRadians(180), driveTrain),
+            // drives to intake cone off ground
+            new DriveForwardGivenDistance(0.3, driveTrain),
+            // lower arm
+            RobotContainer.makeSetPositionArmAndElevatorCommand(ArmConstants.CONE_UP_INTAKE_ANGLE, 0),
+            // intake upright cone from ground
+            new IntakeForGivenTime(intake, IntakeConstants.CONE_IN_SPEED, 0.7),
+            RobotContainer.makeSetPositionArmAndElevatorCommand(ArmConstants.TURTLE_ANGLE, 0),
+            // drive back on charging station
+            new DriveForwardGivenDistance(-2.4, driveTrain),
+            new EngageCmd(driveTrain)
+
+            
+          );
+    }
+
+}
