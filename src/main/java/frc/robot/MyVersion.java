170b6fbea163d31c595925c0b6e0dbb23142cd53
Will Marshall <willcodymarshall@gmail.com>
2023-08-26 18:59:43 -0400

    update to WPILib 2023.4.3

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "2023-Season-2";
+	public static final String MAVEN_NAME = "2023-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 473;
-	public static final String GIT_SHA = "418cf8d04bcc857d7a67fc725db2c187565556bb";
-	public static final String GIT_DATE = "2023-04-18 20:51";
+	public static final int GIT_REVISION = 476;
+	public static final String GIT_SHA = "be48fc1ac20bc6cfef41d48b7f1fcc6bb166285e";
+	public static final String GIT_DATE = "2023-04-21 07:50";
 	public static final String GIT_BRANCH = "worlds";
-	public static final String BUILD_DATE = "2023-04-20 09:20";
-	public static final long BUILD_UNIX_TIME = 1682000457223L;
+	public static final String BUILD_DATE = "2023-08-26 09:53";
+	public static final long BUILD_UNIX_TIME = 1693061635815L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
